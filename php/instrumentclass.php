6d3821d7c280bae7b45313fbf8004f9bf6129b06
rbecker8 <rbecker8@cnm.edu>
2018-07-31 08:56:40 -0600

    Moved name and type classes back to main file.

--- php/instrumentclass.php
+++ php/instrumentclass.php
@@ -8,4 +8,21 @@ class Instrument {
 		$this->setInstrumentName($newInstrumentName);
 		$this->setInstrumentType($newInstrumentType);
 	}
+
+	public function getInstrumentName(): string {
+		return ($this->instrumentName);
+	}
+
+	public function setInstrumentName(string $newInstrumentName): void {
+		$this->instrumentName = $newInstrumentName;
+	}
+
+	public function getInstrumentType(): string {
+		return ($this->instrumentType);
+	}
+
+	public function setInstrumentType(string $newInstrumentType): void {
+		$this->instrumentType = $newInstrumentType;
+	}
 }
+
